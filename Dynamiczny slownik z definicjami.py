6b60ecdba46d70ad8f4bb01d9efd4e551ea4b8fa
Mietaba <100045695+Mietaba@users.noreply.github.com>
2022-02-21 20:24:08 +0100

    warunek istnienia input()

--- Dynamiczny slownik z definicjami.py
+++ Dynamiczny slownik z definicjami.py
@@ -1,17 +1,30 @@
+from errno import EILSEQ
+
+
 definicje = {1:'Marek', 2:'Jacek', 3:'Bartek'}
 print(definicje)
 while True:
-    komenda = input('Powiedz co chcesz zrobic: \n1-dodac (imie, numer), 2-szukac (numer), 3-usuwac (numer), 4-wyswietlic liste, 9-END: ')
+    komenda = input('Powiedz co chcesz zrobic: \n'
+    '1-dodac (imie, numer), 2-szukac (numer), 3-usuwac (numer), 4-wyswietlic liste, 9-END: ')
     if komenda == '1':
         definicje[int(input())] = input()
         print()
     elif komenda == '2':
         szukac = input('ktora pozycje chcesz szukac? ')
-        print(definicje[int(szukac)])
+        szukac = int(szukac)
+        if int(szukac) in definicje:
+            print(definicje[szukac])
+        else:
+            print('Pozycja not exist')
         print()
     elif komenda == '3':
         usunac = input('ktora pozycje chcesz usunac? ')
-        del definicje[int(usunac)]
+        usunac = int(usunac)
+        if usunac in definicje:
+            del definicje[usunac]
+        else:
+            print('Pozycja not exist')
+        print()
     elif komenda == '4':
         for d in definicje:
             print(d, definicje[d])
