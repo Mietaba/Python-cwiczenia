22917593a61dbc187b4c7f482385c04503baf3bf
Mietaba <100045695+Mietaba@users.noreply.github.com>
2022-02-22 07:50:55 +0100

    Zmiana wartosci 1,2,3 na str w celu uproszczenia kodu

--- Dynamiczny slownik z definicjami.py
+++ Dynamiczny slownik z definicjami.py
@@ -1,29 +1,23 @@
-from errno import EILSEQ
-
-
-definicje = {1:'Marek', 2:'Jacek', 3:'Bartek'}
+definicje = {'1':'Marek', '2':'Jacek', '3':'Bartek'}
 print(definicje)
 while True:
-    komenda = input('Powiedz co chcesz zrobic: \n'
-    '1-dodac (imie, numer), 2-szukac (numer), 3-usuwac (numer), 4-wyswietlic liste, 9-END: ')
+    komenda = input('Powiedz co chcesz zrobic: \n1-dodac (imie, numer), 2-szukac (numer), 3-usuwac (numer), 4-wyswietlic liste, 9-END: ')
     if komenda == '1':
-        definicje[int(input())] = input()
+        definicje[input()] = input()
         print()
     elif komenda == '2':
-        szukac = input('ktora pozycje chcesz szukac? ')
-        szukac = int(szukac)
-        if int(szukac) in definicje:
+        szukac = input('Ktora pozycje chcesz szukac? ')
+        if szukac in definicje:
             print(definicje[szukac])
         else:
-            print('Pozycja not exist')
+            print('Pozycja nie istnieje')
         print()
     elif komenda == '3':
-        usunac = input('ktora pozycje chcesz usunac? ')
-        usunac = int(usunac)
+        usunac = input('Ktora pozycje chcesz usunac? ')
         if usunac in definicje:
             del definicje[usunac]
         else:
-            print('Pozycja not exist')
+            print('Pozycja nie istnieje')
         print()
     elif komenda == '4':
         for d in definicje:
@@ -33,7 +27,7 @@ while True:
         break
 
     else:
-        print('zly numer!')
+        print('Wpisales zly numer!')
 
 print()
 for d in definicje:
